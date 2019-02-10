d83c4a04e3989f7743b9ba4581f174def9a20beb
Tadashi Yamasaki <yamasaki@haitekuya.com>
2019-02-10 23:38:01 +0900

    Give token as variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,4 @@ TOKEN=token
 # Search Query
 QUERY="akiyaInitRM.akiyaRefM.checks=14&akiyaInitRM.akiyaRefM.checks=20"
 
-sh /usr/src/app/search.sh $QUERY && sh /usr/src/app/notify.sh
+sh /usr/src/app/search.sh $QUERY && sh /usr/src/app/notify.sh $TOKEN
--- notify.sh
+++ notify.sh
@@ -1,2 +1,6 @@
 
-curl -X POST -H "Authorization: Bearer $TOKEN" -F message="`cat result.out`" https://notify-api.line.me/api/notify
+if [ $# -ne 1 ]; then
+  exit 1
+fi
+
+curl -X POST -H "Authorization: Bearer $1" -F message="`cat result.out`" https://notify-api.line.me/api/notify
